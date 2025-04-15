9de775ccbdd6be997220f5e3324859a41f7d47cf
Leime <67313990+tjwhang@users.noreply.github.com>
2025-04-16 01:16:31 +0900

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ===================== 마더텅 E북 리더 PDF 다운로드 도구 =======================
+echo 만든놈: tjwhang
+echo 확인 사항: Python 3.x 버전이 설치되어 있어야 합니다.
+echo 파이썬 스크립트 실행 중..
+python.exe extractor.py
+pause
